9541dbcef2ba1fa5d6e11b6e06c717ec4f68c5d1
luandly <luandly>
2018-12-27 14:22:02 +0800

    全局application中添加 Logger工具

--- app/src/main/java/com/weblab/adt/weblab/app/IMApplication.java
+++ app/src/main/java/com/weblab/adt/weblab/app/IMApplication.java
@@ -1,7 +1,22 @@
 package com.weblab.adt.weblab.app;
 
+import android.app.Application;
+
+import com.weblab.adt.weblab.utils.Logger;
+
+
 /**
  * 全局的Application 应用起来，先进这里
  */
-public class IMApplication {
+public class IMApplication  extends Application {
+
+    /**
+     * 引入自定义的工具类
+     */
+    private Logger logger = Logger.getLogger(IMApplication.class);
+
+    /**
+     * 先定义一个全局常量，暂时不清楚用在哪里
+     */
+    public static boolean gifRunning = true;//gif是否运行
 }
